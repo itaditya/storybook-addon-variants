686d9be0e4b4d6b1ec8c74de1ab844a324853c0a
Aditya Agarwal <adityaa803@gmail.com>
2022-06-25 12:42:45 +0530

    document btn props & enable detailed table

--- stories/Button.tsx
+++ stories/Button.tsx
@@ -2,9 +2,13 @@ import React, { ReactNode } from "react";
 import "./button.css";
 
 export interface Props {
+  /** Activating the button would lead to what type of result */
   result?: "neutral" | "positive" | "negative";
+  /** Size of the button */
   size?: "small" | "normal" | "large";
+  /** Show solid bg or transparent bg with outline button */
   outline?: boolean;
+  /** Content of the button */
   children: ReactNode;
 }
 
